0bb465a08825188015438346d8fd5dd8cc08dfc6
Jyothi-hutagi <jyothihutagi@gmail.com>
2022-01-29 20:09:00 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import Home from './Components/Home';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import StudentDashboard from './Components/StudentDashboard'
+import Quiz from './Components/Questionhtml';
+import Quiz1 from './Components/Questioncss';
+import Quiz2 from './Components/Questionjs';
+import Answer from './Components/Answer';
+import Question from './Components/Question';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className='app'>
+        <Switch>
+         <Route path ="/" exact Component={Home}>
+           <Home/>
+         </Route>
+         <Route path="/Login"exact Component={Login}>
+           <Login/>
+         </Route>
+         <Route path="/Components/Signup.js" exact Component={Signup}>
+           <Signup/>
+         </Route>
+         <Route path="/StudentDashboard" exact  Component={StudentDashboard}>
+           <StudentDashboard/>
+         </Route>
+         <Route path="/html" exact  Component={Quiz}>
+           <Quiz/>
+         </Route>
+         <Route path="/css" exact  Component={Quiz1}>
+           <Quiz1/>
+         </Route>
+         <Route path="/js" exact  Component={Quiz1}>
+           <Quiz2/>
+         </Route>
+         <Route path="/Components/Answer.js" exact  Component={Answer}>
+           <Answer/>
+         </Route>
+         <Route path="/Components/Question.js" exact  Component={Question}>
+           <Question/>
+         </Route>
+      </Switch>
     </div>
+    </Router>
+   
   );
 }
 
